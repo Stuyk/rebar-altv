7e635aacda62a2e280b00207f078057def8b373c
stuyk <trevorwessel@protonmail.com>
2024-05-09 12:50:24 -0600

    feat: add tailwind anims, and update deps

--- webview/tailwind.config.js
+++ webview/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', '../src/plugins/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                fadein: 'fadeIn .5s ease-in-out',
+                fadeout: 'fadeOut .5s ease-in-out',
+            },
+            keyframes: {
+                fadeIn: {
+                    from: { opacity: 0 },
+                    to: { opacity: 1 },
+                },
+                fadeOut: {
+                    from: { opacity: 1 },
+                    to: { opacity: 0 },
+                },
+            },
+        },
     },
     plugins: [],
 };
