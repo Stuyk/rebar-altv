dc163cf24a8ab543e9fe92111f3aacb83e8d5765
Ivan Boldyrev <iboldurev@gmail.com>
2024-05-12 01:40:52 +0300

    added ukrainian translation

--- src/main/translate/index.ts
+++ src/main/translate/index.ts
@@ -7,4 +7,8 @@ setBulk({
         'system.server.started': 'Server Started Successfully',
         'controllers.interaction.message': `Press 'E' to Interact`,
     },
+    uk: {
+        'system.server.started': 'Сервер успішно запущено',
+        'controllers.interaction.message': 'Натисніть \'E\', для взаємодії',
+    }
 });
