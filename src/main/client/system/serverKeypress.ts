098b415510cdad63669e6d1acf51517112f9c964
KoronGIT <error@dragonspace.de>
2024-07-26 15:12:42 +0200

    Add new callback "hold" to useKeypress

--- src/main/client/system/serverKeypress.ts
+++ src/main/client/system/serverKeypress.ts
@@ -6,9 +6,13 @@ const messenger = useMessenger();
 const validKeys: Map<number, boolean> = new Map();
 const nextValidUpPress: { [key: string]: number } = {};
 const nextValidDownPress: { [key: string]: number } = {};
+const keyStates: { [key: number]: { isPressed: boolean; pressStartTime: number; pressDuration: number } } = {};
+const requiredPressDuration = 2000;
+
 
 function handleUpdate(keys: string[]) {
     validKeys.clear();
+
 
     for (let key of keys) {
         validKeys.set(parseInt(key), true);
@@ -36,6 +40,10 @@ function handleKeyup(key: number) {
         return;
     }
 
+    if (keyStates[key]) {
+        keyStates[key].isPressed = false;
+    }
+
     nextValidUpPress[key] = Date.now() + 500;
     alt.emitServer(Events.systems.keypress.invokeUp, key);
 }
@@ -61,6 +69,12 @@ function handleKeyDown(key: number) {
         return;
     }
 
+    if (!keyStates[key]) {
+        keyStates[key] = { isPressed: false, pressStartTime: 0, pressDuration: 0 };
+    }
+
+    keyStates[key].isPressed = true;
+    keyStates[key].pressStartTime = Date.now();
     nextValidDownPress[key] = Date.now() + 500;
     alt.emitServer(Events.systems.keypress.invokeDown, key);
 }
@@ -68,3 +82,26 @@ function handleKeyDown(key: number) {
 alt.onServer(Events.systems.keypress.update, handleUpdate);
 alt.on('keyup', handleKeyup);
 alt.on('keydown', handleKeyDown);
+
+
+alt.everyTick(() => {
+    const currentTime = Date.now();
+
+    for (const key in keyStates) {
+        const state = keyStates[key];
+
+        if (state.isPressed) {
+            state.pressDuration = currentTime - state.pressStartTime;
+
+            if (state.pressDuration >= requiredPressDuration) {
+                state.isPressed = false;
+                state.pressStartTime = 0;
+                state.pressDuration = 0;
+                alt.emitServer(Events.systems.keypress.invokeHold, parseInt(key));
+            }
+        } else {
+            state.pressStartTime = 0;
+            state.pressDuration = 0;
+        }
+    }
+});
