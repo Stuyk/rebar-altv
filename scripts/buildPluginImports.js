0048b7cd3777905c13b8eb4c51867f3daf78af6d
KoronGIT <error@dragonspace.de>
2024-08-03 18:11:09 +0200

    Update buildPluginImports.js
    remove wrong commit

--- scripts/buildPluginImports.js
+++ scripts/buildPluginImports.js
@@ -34,42 +34,6 @@ async function start() {
             serverFolders.push(pluginFolder + '/server');
         }
     }
-
-    const APIMap = new Map();
-
-    for (let serverFolder of serverFolders) {
-        if (fs.existsSync(serverFolder + '/api.ts')) {
-            const file = fs.readFileSync(serverFolder + '/api.ts', 'utf8');
-
-            const regex = /Rebar\.useApi\(\)\.register\(([^,]+), useApi\(\)\);/;
-            const match = file.match(regex);
-
-            if (match && match[1]) {
-                let extractedContent = match[1].trim();
-                if ((extractedContent.startsWith('"') && extractedContent.endsWith('"')) || (extractedContent.startsWith("'") && extractedContent.endsWith("'"))) {
-                    extractedContent = extractedContent.replace(/["']/g, '');
-                } else {              
-                    const regexApiConst = new RegExp(`const\\s+${extractedContent}\\s*=\\s*['"]([^'"]+)['"]\\s*;`);
-                    const matchApiConst = file.match(regexApiConst);
-                    if (matchApiConst && matchApiConst[1]) {
-                        extractedContent = matchApiConst[1].trim();
-                        extractedContent = extractedContent.replace(/["']/g, '');
-                    }
-                }
-
-                const regexFunctions = /return\s*{([^}]+)}/;
-                const matchFunctions = file.match(regexFunctions);
-                if (matchFunctions && matchFunctions[1]) {
-                    let extractedContentFunction = matchFunctions[1].trim();
-                    let functionArray = extractedContentFunction.split(',').map(func => func.trim());
-                    APIMap.set(extractedContent, functionArray);
-                } else
-                {
-                    APIMap.set(extractedContent, functionArray);
-                }
-            } 
-        }
-    }
     
 
     // Propogate server import paths
@@ -81,45 +45,9 @@ async function start() {
 
         const folderSlice = serverFolder.split('/');
         const folderName = folderSlice[folderSlice.length - 2];
-        let missing = false;
-
-        if (fs.existsSync(serverFolder + '/dependencies.json')) {
-            const file = fs.readFileSync(serverFolder + '/dependencies.json'); 
-            try {
-                /** @type {{ api?: { [key: string]: string[] }}} */
-                const data = JSON.parse(file);
-                if (!data.api) {
-                    continue;
-                }
-    
-                for (const apiKey in data.api) {
-                    if (!APIMap.has(apiKey)) {
-                        fs.appendFileSync(serverImportsPath, `alt.logError('::: Plugin: ${folderName} - Dependent API missing: ${apiKey}');` + '\r\n');
-                        missing = true;
-                        continue;
-                    }
-
-
-                    if (data.api.hasOwnProperty(apiKey)) {
-                       for (const func of data.api[apiKey]) {
-                            if (!APIMap.get(apiKey).includes(func)) {
-                                fs.appendFileSync(serverImportsPath, `alt.logError('::: Plugin: ${folderName} - Dependent API function missing: ${apiKey} - ${func}');` + '\r\n');
-                                missing = true;
-                            }
-                        }
-                       
-                    }
-                }
-            } catch (err) {
-                console.warn(`Could not read, is JSON file valid? ${serverFolder}`);
-            } 
-        }
-
-        if (missing === false) {
-            fs.appendFileSync(serverImportsPath, `alt.log('::: Plugin: ${folderName}');` + '\r\n');
-            const importLine = `import '${path}';`;
-            fs.appendFileSync(serverImportsPath, importLine + '\r\n');  
-        }
+        fs.appendFileSync(serverImportsPath, `alt.log('::: Plugin: ${folderName}');` + '\r\n');
+        const importLine = `import '${path}';`;
+        fs.appendFileSync(serverImportsPath, importLine + '\r\n');
     }
 
     // Propogate client import paths
