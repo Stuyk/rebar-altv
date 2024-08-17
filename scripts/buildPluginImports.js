25bdb09ac9ad4efc51429f49c1c54899ca324917
Liam Robinson <liamrobinson5@web.de>
2024-08-17 16:29:32 +0200

    POC: Added Plugin Categories. (Closes #142)
    Only a Proof of Concept. I am pretty sure there are things to optimize or even something i have forgot.

--- scripts/buildPluginImports.js
+++ scripts/buildPluginImports.js
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import glob from 'fast-glob';
+import path from 'path';
 
 const serverImportsPath = './resources/core/main/server/plugins.js';
 const clientImportsPath = './resources/core/main/client/plugins.js';
@@ -17,7 +18,6 @@ function getIndexPath(folder) {
 
 async function start() {
     const pluginFolders = await glob('./src/plugins/*', options);
-
     const serverFolders = [];
     const clientFolders = [];
 
@@ -26,39 +26,47 @@ async function start() {
             continue;
         }
 
-        if (fs.existsSync(pluginFolder + '/client')) {
-            clientFolders.push(pluginFolder + '/client');
-        }
-
-        if (fs.existsSync(pluginFolder + '/server')) {
-            serverFolders.push(pluginFolder + '/server');
+        if (path.basename(pluginFolder).startsWith('[') && path.basename(pluginFolder).endsWith(']')) {
+            const category = path.basename(pluginFolder).slice(1, -1);
+            const categoryPlugins = await glob(pluginFolder + '/*', options);
+            for (let categoryPlugin of categoryPlugins) {
+                if (fs.existsSync(categoryPlugin + '/client')) {
+                    clientFolders.push({ folder: categoryPlugin + '/client', category });
+                }
+                if (fs.existsSync(categoryPlugin + '/server')) {
+                    serverFolders.push({ folder: categoryPlugin + '/server', category });
+                }
+            }
+        } else {
+            if (fs.existsSync(pluginFolder + '/client')) {
+                clientFolders.push({ folder: pluginFolder + '/client', category: 'Uncategorized' });
+            }
+            if (fs.existsSync(pluginFolder + '/server')) {
+                serverFolders.push({ folder: pluginFolder + '/server', category: 'Uncategorized' });
+            }
         }
     }
 
-    // Propogate server import paths
-    for (let serverFolder of serverFolders) {
+    for (let { folder: serverFolder, category } of serverFolders) {
         const path = getIndexPath(serverFolder);
         if (!path) {
             continue;
         }
-
         const folderSlice = serverFolder.split('/');
         const folderName = folderSlice[folderSlice.length - 2];
-        fs.appendFileSync(serverImportsPath, `alt.log('::: Plugin: ${folderName}');` + '\r\n');
+        fs.appendFileSync(serverImportsPath, `alt.log('::: Plugin: ${folderName} | Category: ${category}');` + '\r\n');
         const importLine = `import '${path}';`;
         fs.appendFileSync(serverImportsPath, importLine + '\r\n');
     }
 
-    // Propogate client import paths
-    for (let clientFolder of clientFolders) {
+    for (let { folder: clientFolder, category } of clientFolders) {
         const path = getIndexPath(clientFolder);
         if (!path) {
             continue;
         }
-
         const folderSlice = clientFolder.split('/');
         const folderName = folderSlice[folderSlice.length - 2];
-        fs.appendFileSync(clientImportsPath, `alt.log('::: Plugin: ${folderName}');` + '\r\n');
+        fs.appendFileSync(clientImportsPath, `alt.log('::: Plugin: ${folderName} | Category: ${category}');` + '\r\n');
         const importLine = `import '${path}';`;
         fs.appendFileSync(clientImportsPath, importLine + '\r\n');
     }
