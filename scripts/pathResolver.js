10b8f500588b06c27f47672e5e68b68ca86782a9
Ilya Sereb <43453247+isereb@users.noreply.github.com>
2024-10-07 10:13:23 -0400

    path resolver (#166)

--- scripts/pathResolver.js
+++ scripts/pathResolver.js
@@ -10,6 +10,7 @@ function updatePaths(path) {
     file = file.replace(/\@Server/gm, `${relativePath}main/server`);
     file = file.replace(/\@Client/gm, `${relativePath}main/client`);
     file = file.replace(/\@Shared/gm, `${relativePath}main/shared`);
+    file = file.replace(/\@Plugins/gm, `${relativePath}plugins`);
     fs.writeFileSync(path, file);
 }
 
